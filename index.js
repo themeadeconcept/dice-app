dbd1e3559e148be70d812b78679ef86c134a61da
themeadeconcept <49075241+themeadeconcept@users.noreply.github.com>
2020-12-01 18:33:16 -0500

    Simple dice application to practice some more JavaScript

---
+++ index.js
@@ -0,0 +1,23 @@
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //generates a random number between 0-5, +1 makes 1-6
+
+  var randomDiceImage = "dice" + randomNumber1 + ".png";//dice1.png - dice6.png
+  var randomImageSource = "images/" + randomDiceImage;//images/dice1.png-images/dice6.png
+  var image1 = document.querySelectorAll("img")[0];
+
+  image1.setAttribute("src", randomImageSource);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1; //generates a random number between 0-5, +1 makes 1-6
+
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "PLAYER 1 is the WINNER!";
+  }
+  else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "PLAYER 2 is the WINNER!";
+  }
+  else {
+    document.querySelector("h1").innerHTML = "DRAW!";
+  }
